1b9fdcc3ea15eef0b0ced7906629777fce17eb3c
Dariusz Okonski <darek200180@gmail.com>
2022-05-08 12:28:50 +0100

    build App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
 import ReactDom from 'react-dom';
 
 const App = () => {
-    return <h1>hi</h1>
+    const [input, setInput] = useState<string>('')
+    const [code, setCode] = useState<string>('')
+
+    const onClick = () => {
+        console.log(input)
+    }
+
+    return (
+        <div>
+            <textarea
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            ></textarea>
+
+            <div>
+                <button
+                    onClick={onClick}
+                >Submit</button>
+            </div>
+
+            <pre>{code}</pre>
+        </div>
+    )
 }
 
 ReactDom.render(
