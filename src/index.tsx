70d9fa0343336a4bf5d704432f953deb2d44bcb0
Dariusz Okonski <darek200180@gmail.com>
2022-05-09 18:26:10 +0100

    some errors occured

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import { useEffect, useRef, useState } from 'react';
 import ReactDom from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 
 const App = () => {
@@ -27,12 +28,16 @@ const App = () => {
             return;
         }
         
-        const result = await ref.current.transform(input, {
-            loader: 'jsx',
-            target: 'es2015'
-        });
+       const result = await ref.current.build({
+           entryPoints: ['index.js'],
+           bundle: true,
+           write: false,
+           plugins: [unpkgPathPlugin()]
+       });
 
-        setCode(result.code)
+       console.log(result.outpuFiles[0].text);
+
+       setCode(result.outpuFiles[0].text)
     }
 
     return (
